2dd5cb560ce4dec5f7e8f8834183fcdf8bb36a2b
ChillyMedina <73111027+ChillyMedina@users.noreply.github.com>
2020-11-01 02:36:27 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+  document.getElementById("nameButton")
+        .addEventListener("click", function() {
+    /*GUARDAR NOMBRE*/
+    document.querySelector("#nombre-usuario").innerHTML = document.getElementById("nombre").value;
+  document.getElementById("nombreblah").hidden = true;
+  document.getElementById("offset").hidden = false;
+}, false);
+
+
+  /*LISTA*/
+    var boton = document.querySelector("#boton"),
+    texto = document.querySelector("#valor"),
+    lista = document.querySelector("#listaChilly");
+    boton.onclick = (e) => {
+      var li = document.createElement("li");
+      li.appendChild(document.createTextNode(texto.value));
+      lista.appendChild(li);
+    }
