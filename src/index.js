d6706cc34113e4efb903608557ff56c0517bd2f0
ChillyMedina <73111027+ChillyMedina@users.noreply.github.com>
2020-11-12 00:47:27 -0300

    comentarios css/js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import cipher from "./cipher.js";
 
 var offset; //se declara variable offset para que guarde numero en la segunda pantalla y poder utilizarlo con el cifrado mas adelante
 //---------------GUARDAR NOMBRE---------------
-const sectionOne = document.getElementById("nombre");
+const sectionOne = document.getElementById("nombre"); //se declara constante para cada section
 const sectionTwo = document.getElementById("numero");
 const sectionThree = document.getElementById("lista");
 
@@ -11,8 +11,9 @@ document.getElementById("namebtn").onclick = function () { //funcion onclick par
   let nombreDeUsuario = input.value;
   let nombreUsuario = document.getElementById("nombre-usuario"); //y muestra el valor del input a traves del "span" en la siguiente pantalla
   nombreUsuario.innerHTML = nombreDeUsuario;
-  sectionOne.classList.remove("activo")
-  sectionTwo.classList.add("activo")
+  //---------------PASAR PAGINA 1 a 2---------------
+  sectionOne.classList.remove("activo") //a sectionOne "nombre" se le quita la clase activo que ya tiene asignada en HTML y en CSS(con ciertos valores)
+  sectionTwo.classList.add("activo") //a sectionTwo "numero" se le añade la clase activo que esta en CSS como #numero.activo y se muestran los valores ingresados.
  
 }
 
@@ -21,8 +22,9 @@ document.getElementById("numberbutton").onclick = () => { // //funcion onclick p
   let input = document.getElementById("quantity"); //toma el valor del input "quantity"
   let nombreDeUsuario = input.value;
   offset = nombreDeUsuario //variable declarada en un comienzo para guardar numero de offset y usarlo con cifrado
-  sectionTwo.classList.remove("activo")
-  sectionThree.classList.add("activo")
+//---------------PASAR PAGINA 2 a 3---------------  
+  sectionTwo.classList.remove("activo") //a sectionTwo "numero" se le quita la clase activo que ya tiene asignada en CSS(con ciertos valores)
+  sectionThree.classList.add("activo") //a sectionThree "lista" se le añade la clase activo que esta en CSS como #lista.activo y se muestran los valores ingresados.
 }
 
 //---------------LISTA---------------
@@ -33,5 +35,5 @@ boton.onclick = (e) => { //se le asigna un evento (e) onclick al boton
   var li = document.createElement("li"); // el evento crea un elemento de tipo "li"(lista dentro de un "ul"(unordered list))
   li.appendChild(document.createTextNode(texto.value)); //a "li" se le agrega un nuevo elemento de tipo texto con el valor de la variable text
   lista.appendChild(li); //a lista se le agrega "li"
-  texto.value=""
+  texto.value="" //se deja vacia la variable texto al crear un nuevo "li"
 }
