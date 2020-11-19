0be92d33b7a59b4e2eab7265d24222031bf3008d
ChillyMedina <73111027+ChillyMedina@users.noreply.github.com>
2020-11-19 08:46:31 -0300

    Ultimas modificaciones

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import cipher from "./cipher.js";
 
-var offset; //se declara variable offset para que guarde numero en la segunda pantalla y poder utilizarlo con el cifrado mas adelante
+let offset; //se declara variable offset para que guarde numero en la segunda pantalla y poder utilizarlo con el cifrado mas adelante
 //---------------GUARDAR NOMBRE---------------
 const sectionOne = document.getElementById("nombre"); //se declara constante para cada section
 const sectionTwo = document.getElementById("numero");
 const sectionThree = document.getElementById("lista");
 
 document.getElementById("namebtn").onclick = function () { //funcion onclick para el boton "namebtn"
-  let input = document.getElementById("nombretxt"); //toma el valor del input nombretxt
+  let input = document.getElementById("nombretxt"); //variable toma el valor del input nombretxt
   let nombreDeUsuario = input.value;
   let nombreUsuario = document.getElementById("nombre-usuario"); //y muestra el valor del input a traves del "span" en la siguiente pantalla
   nombreUsuario.innerHTML = nombreDeUsuario;
@@ -28,12 +28,13 @@ document.getElementById("numberbutton").onclick = () => { // //funcion onclick p
 }
 
 //---------------LISTA---------------
-var boton = document.querySelector("#boton"),
+let boton = document.querySelector("#boton"),
   texto = document.querySelector("#valor"),
   lista = document.querySelector("#listaChilly"); //se declaran 3 variables que son los inputs(a traves de los ID)
 boton.onclick = (e) => { //se le asigna un evento (e) onclick al boton
-  var li = document.createElement("li"); // el evento crea un elemento de tipo "li"(lista dentro de un "ul"(unordered list))
+  let li = document.createElement("li"); // el evento crea un elemento de tipo "li"(lista dentro de un "ul"(unordered list))
   li.appendChild(document.createTextNode(texto.value)); //a "li" se le agrega un nuevo elemento de tipo texto con el valor de la variable text
   lista.appendChild(li); //a lista se le agrega "li"
   texto.value="" //se deja vacia la variable texto al crear un nuevo "li"
 }
+
