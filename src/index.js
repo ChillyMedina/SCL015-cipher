dab4bf25b656f53b2e3527a022e760082d2ff07c
ChillyMedina <73111027+ChillyMedina@users.noreply.github.com>
2020-11-23 09:29:33 -0300

    Ultimas Modificaciones

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import cipher from "./cipher.js";
+/*import cipher from "./cipher.js";*/
 
 let offset; //se declara variable offset para que guarde numero en la segunda pantalla y poder utilizarlo con el cifrado mas adelante
 //---------------GUARDAR NOMBRE---------------
@@ -36,5 +36,54 @@ boton.onclick = (e) => { //se le asigna un evento (e) onclick al boton
   li.appendChild(document.createTextNode(texto.value)); //a "li" se le agrega un nuevo elemento de tipo texto con el valor de la variable text
   lista.appendChild(li); //a lista se le agrega "li"
   texto.value="" //se deja vacia la variable texto al crear un nuevo "li"
+}
+
+function cipher() {
+  //Declaro la variable del mensaje ingresado, llamo al valor y lo paso a mayúsculas
+  let messageIn = document.getElementById("containerOne").value.toUpperCase();
+  //Convierto el desplazamiento a numero entero
+  let offset = parseInt(document.getElementById("offset").value);
+
+  let message = ""
+  //Inicio el bucle, declaro e inicializo la variable i, que será menor al largo del mensaje, incrementado a un valor
+  for (let i = 0; i < messageIn.length; i++) {
+      //Pasamos el mensaje a código ASCII
+      let messageAscii = messageIn.charCodeAt(i);
+      // console.log(1, messageAscii)
+      //Aplicamos la formula para obtener la nueva posicion de la letra
+      let letterNewPosition = (messageAscii - 65 + offset) % 26 + 65;
+      // console.log(2, letterNewPosition)
+      //Si el valor es estrictamente igual a 32 (espacio en ASCII) le digo a la variable que mantenga el mismo valor
+      if (messageAscii === 32) { letterNewPosition = 32; }
+      //Convierto el ASCII a la nueva posición
+      let sinceAscii = String.fromCharCode(letterNewPosition);
+      // console.log(3, sinceAscii)
+      //concateno las variables 
+      message += sinceAscii;
+      console.log(4, message)
+  }
+  document.getElementById("containerTwo").innerHTML = message;
+
+  return message;
+}
+
+
+function decode () {
+  let messageOf = document.getElementById("containerTwo").value.toUpperCase();
+  let offset = parseInt(document.getElementById("offset").value);
+  let messageOut = ""
+  for (let i = 0; i < messageOf.length; i++) {
+      let messageAsciiOf = messageOf.charCodeAt(i);
+      let letterNewPositionOf = (messageAsciiOf + 65 - offset) % 26 + 65;
+      if (messageAsciiOf === 32) { letterNewPositionOf = 32; }
+      let sinceAsciiOf = String.fromCharCode(letterNewPositionOf);
+      messageOut += sinceAsciiOf;
+      console.log(messageOut)
+}
+
+document.getElementById("containerTwo").innerHTML = messageOut;
+
+return messageOut;
+
 }
 
